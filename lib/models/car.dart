9e9b0ac34779ba35e3001c8bc74da0f1d940a3c3
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-01-20 23:38:56 +0300

    first commit

---
+++ lib/models/car.dart
@@ -0,0 +1,9 @@
+//import 'package:flutter_sqflite_cars/utils/dbhelper.dart';
+
+class Car {
+  int id;
+  String name;
+  int miles;
+
+  Car(this.id, this.name, this.miles);
+}
---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_sqflite_cars/models/car.dart';
+//import 'package:flutter_sqflite_cars/utils/dbhelper.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
