0065dcb4a906342b6da7b978c3292ce0ca565b67
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-01-20 23:58:47 +0300

    Mapler yazıldı.

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -1,9 +1,23 @@
-//import 'package:flutter_sqflite_cars/utils/dbhelper.dart';
+import 'package:flutter_sqflite_cars/utils/dbhelper.dart';
 
 class Car {
-  int id;
-  String name;
-  int miles;
+  int? id;
+  String? name;
+  int? miles;
 
   Car(this.id, this.name, this.miles);
+
+  Car.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    miles = map['miles'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      DatabaseHelper.columnId: id,
+      DatabaseHelper.columnName: name,
+      DatabaseHelper.columnMiles: miles,
+    };
+  }
 }
